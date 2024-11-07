e730065280c7b641c03135bed6120621b7b1295b
CurtisIkennaAgha <curtisikennaagha@gmail.com>
2024-11-07 15:34:12 +0000

    login

---
+++ config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBgQgASCK3qw1z9esdLO6Ufzpejde-SfAk",
+    authDomain: "hoop-theory.firebaseapp.com",
+    projectId: "hoop-theory",
+    storageBucket: "hoop-theory.firebasestorage.app",
+    messagingSenderId: "18845490927",
+    appId: "1:18845490927:web:e7d5ad4020122c56fc451a",
+    measurementId: "G-NZXJ0K990H"
+};
+export {firebaseConfig};
